f117b26113c28200e6b244dd44da0249c8d31bc7
Skip Sopscak <dsopscak@cox.net>
2014-12-17 18:19:21 -0600

    Add vagrant version of make_builduser function

--- vagrant_helper.sh
+++ vagrant_helper.sh
@@ -16,3 +16,13 @@ set_nameserver()
     # nameserver setting.
     echo "PEERDNS=no" | sudo tee -a /etc/sysconfig/network-scripts/ifcfg-eth0
 }
+
+make_builduser()
+{
+    local the_user=$1
+
+    # Add the user to the vagrant group which should make
+    # vagrant-mounted shares writable for him. Assuming staging areas
+    # will be in such shares, allows build user to stage stuff.
+    sudo sed -e "s/^vagrant:.*$/&$the_user/" -i.orig /etc/group
+}
