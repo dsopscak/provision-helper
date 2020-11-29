14f4a7b41d9d26e0bd273ff1b7833dba7b4981aa
Skip Sopscak <dsopscak@gmail.com>
2020-11-29 11:02:33 -0600

    handle chef license acceptance

--- install_chef_client.sh
+++ install_chef_client.sh
@@ -2,8 +2,31 @@
 #
 # install_chef_client
 
-VERSION=16.7.61-1
-RPM=chef-${VERSION}.el7.x86_64.rpm
+VERSION=16.7.61
+RPM=chef-${VERSION}-1.el7.x86_64.rpm
+
+mkdir -p ~/.chef/accepted_licenses
+tee ~/.chef/accepted_licenses/chef_infra_client <<EOT
+---
+id: infra-client
+name: Chef Infra Client
+date_accepted: '2020-11-29T16:48:07+00:00'
+accepting_product: infra-client
+accepting_product_version: $VERSION
+user: vagrant
+file_format: 1
+EOT
+
+tee ~/.chef/accepted_licenses/inspec <<EOT
+---
+id: inspec
+name: Chef InSpec
+date_accepted: '2020-11-29T16:48:07+00:00'
+accepting_product: infra-client
+accepting_product_version: $VERSION
+user: vagrant
+file_format: 1
+EOT
 
 if [[ ! -f $RPM ]]; then
     wget https://packages.chef.io/files/stable/chef/16.7.61/el/8/$RPM
